889731aec233c74c4bd1b1c68f7ac06a9c00d8ce
Teodor Niżyński <teodornizynski@pc26.home>
2016-10-15 20:36:56 +0200

    LogInLiewController basics

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Teodor Niżyński on 15.10.2016.
+//  Copyright © 2016 Teodor Niżyński. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginWithFBButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
