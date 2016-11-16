d502ba9ae43aa56964fafb84edea2c0a2e15cb6c
Teodor Niżński <teodor.nizynski@gmail.com>
2016-11-16 15:04:00 +0100

    updated map so click on pin opens browser

--- OnTheMap/MapViewController.swift
+++ OnTheMap/MapViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     var tabController: TabBarController?
+    var mapAnnotations:[MKAnnotation]=[MKAnnotation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,21 @@ class MapViewController: UIViewController {
         //centerMapOnLocation(location: testLocation)
         
         //navigationController?.navigationBar
+        for _ in 0...200 {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate=CLLocationCoordinate2D(latitude: CLLocationDegrees(arc4random_uniform(100)), longitude: CLLocationDegrees(arc4random_uniform(100)))
+            annotation.title="TestTitle"
+            annotation.subtitle="https://www.youtube.com/watch?v=D4oFSOjSkl8"
+            mapAnnotations.append(annotation)
+        }
+        let annotation = MKPointAnnotation()
+        annotation.coordinate=CLLocationCoordinate2D(latitude: 51.11, longitude: 17.05)
+        annotation.title="TeoTest"
+        annotation.subtitle="https://www.google.pl/maps/@51.112473,17.0562557,16.41z/data=!4m2!10m1!1e2?hl=pl"
+        mapAnnotations.append(annotation)
+        
+        mapView.addAnnotations(mapAnnotations)
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -43,6 +59,36 @@ class MapViewController: UIViewController {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
     }
+    
+    //fucntions from Udacity's PinSample:
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.openURL(URL(string: toOpen)!)
+            }
+        }
+    }
+    
     
     /*
      Additional notes:
